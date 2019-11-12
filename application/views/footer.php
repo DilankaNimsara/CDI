f601127e3ffb3488a8cd901c8cf82dbe9b102ddf
DilankaNimsara <dilankanimsara101@gmail.com>
2019-11-12 23:42:12 +0530

    change footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,37 +1,60 @@
-<!--<footer class="container-fluid text-center ">-->
-<!--	<div class="footer-copyright text-center py-3"> Copyright © 2019 : MrDoc-->
-<!--		<p></p>-->
-<!--	</div>-->
-<!--</footer>-->
-
-<footer>
-	<div class="container">
-		<div class="row">
-			<div class="text-center copyright">
-				<div class="col-sm-1">
-				</div>
-				<div class="col-sm-10">
-				<table align="center">
-					<tr>
-						<td>
-							<img width="100px" src="<?php echo base_url();?>public/img/MR2.png"><br/>
-						</td>
-						<td width="10px"></td>
-						<td>
-							&copy; Copyright - MrDoc.<br/>
-							2019<br/>
-							All Rights Reserved
-						</td>
-					</tr>
-				</table>
-				</div>
-				<div class="col-sm-1">
-					<p align="right">
-						<a href="#"><font color="white">Top</font></a>
-					</p>
-				</div>
-
-			</div>
-	</div>
+<footer class="container-fluid text-center ">
+	<div class="footer-copyright text-center py-3">
+		<div class="col-sm-1">
+			<p align="right">
+			</p>
+		</div>
+		<div class="col-sm-10">
+		<table align="center">
+			<tr>
+				<td>
+					<img width="100px" src="<?php echo base_url();?>public/img/MR2.png"><br/>
+				</td>
+				<td width="10px"></td>
+				<td>
+					&copy; Copyright - MrDoc.<br/>
+					2019<br/>
+					All Rights Reserved
+				</td>
+			</tr>
+		</table>
+		</div>
+		<div class="col-sm-1">
+			<p align="right">
+				<a href="#"><font color="white">Top</font></a>
+			</p>
+		</div>
 	</div>
 </footer>
+<!---->
+<!--<footer>-->
+<!--	<div class="container">-->
+<!--		<div class="row">-->
+<!--			<div class="text-center copyright">-->
+<!--				<div class="col-sm-1">-->
+<!--				</div>-->
+<!--				<div class="col-sm-10">-->
+<!--				<table align="center">-->
+<!--					<tr>-->
+<!--						<td>-->
+<!--							<img width="100px" src="--><?php //echo base_url();?><!--public/img/MR2.png"><br/>-->
+<!--						</td>-->
+<!--						<td width="10px"></td>-->
+<!--						<td>-->
+<!--							&copy; Copyright - MrDoc.<br/>-->
+<!--							2019<br/>-->
+<!--							All Rights Reserved-->
+<!--						</td>-->
+<!--					</tr>-->
+<!--				</table>-->
+<!--				</div>-->
+<!--				<div class="col-sm-1">-->
+<!--					<p align="right">-->
+<!--						<a href="#"><font color="white">Top</font></a>-->
+<!--					</p>-->
+<!--				</div>-->
+<!---->
+<!--			</div>-->
+<!--	</div>-->
+<!--	</div>-->
+<!--</footer>-->
