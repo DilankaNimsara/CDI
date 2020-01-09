6fb8229b66fbaa71a56d1239c752a9f7d9076d03
DilankaNimsara <dilankanimsara101@gmail.com>
2020-01-09 09:52:05 +0530

    foggoten password pop up

--- application/views/login.php
+++ application/views/login.php
@@ -37,7 +37,7 @@
 						<span class="text-danger"><?php echo form_error('password')?></span>
 					</div>
 					<div class="form-group form-check">
-						<a href="#">Forgot Password?</a>
+						<a data-toggle="modal" data-target="#myModal">Forgot Password?</a>
 					</div>
 					<center><button type="submit" class="btn btn-primary" name="submit" value="submit">Login</button></center>
 					<hr/>
@@ -49,6 +49,36 @@
 			}
 		?>
 			</form>
+
+<!--sign up pop up-------------------------------------------------------------------------------------------------------------------------->
+
+			<div class="modal fade" id="myModal" role="dialog">
+				<div class="modal-dialog">
+
+					<div class="modal-content">
+						<div class="modal-header">
+							<button type="button" class="close" data-dismiss="modal">&times;</button>
+							<h4 class="modal-title">Fogotten Password</h4>
+						</div>
+						<div class="modal-body">
+						<!-- text fileds mekata dala form ekak hdnna
+
+
+						-->
+							content
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+
+
+<!--end----------------------------------------------------------------------------------------------------------------------------------------------------->
+
 
 		</div>
 	</div>
